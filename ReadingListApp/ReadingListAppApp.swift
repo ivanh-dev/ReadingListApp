b9d2d764d177a05dabb1abf7aa13fe849d74bf06
Ivan Hirsinger <ivan.hirsinger@gmail.com>
2022-06-23 10:40:20 +0200

    Initial Commit

---
+++ ReadingListApp/ReadingListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingListAppApp.swift
+//  ReadingListApp
+//
+//  Created by Ivan Hirsinger on 23.06.2022..
+//
+
+import SwiftUI
+
+@main
+struct ReadingListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
